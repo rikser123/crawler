28cdcde99a25b23e74d7933f8ae2b42894c9c59c
Leonid Shikhalkin <rikser123@yandex.ru>
2026-06-13 19:17:11 +0300

    kafka message validation

--- src/main/java/rikser123/crawler/consumer/RequestConsumer.java
+++ src/main/java/rikser123/crawler/consumer/RequestConsumer.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import rikser123.bundle.component.ConstraintValidator;
 import rikser123.crawler.dto.KafkaMessageRequestResultDto;
+
 
 @Component
 @RequiredArgsConstructor
@@ -14,11 +16,14 @@ public class RequestConsumer {
   private static final String REQUEST_TOPIC = "REQUEST";
 
   private final ObjectMapper objectMapper;
+  private final ConstraintValidator validator;
 
   @KafkaListener(topics = { REQUEST_TOPIC }, groupId = "crawler")
   public void requestListener(String message) {
     try {
       var data = objectMapper.readValue(message, KafkaMessageRequestResultDto.class);
+      validator.validate(data);
+
       log.info("data {}", data);
     } catch (Exception e) {
       log.warn("ex", e);
--- src/main/java/rikser123/crawler/dto/KafkaMessageRequestResultDto.java
+++ src/main/java/rikser123/crawler/dto/KafkaMessageRequestResultDto.java
@@ -1,8 +1,11 @@
 package rikser123.crawler.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import rikser123.bundle.validation.CheckSqlInjection;
 
 import java.util.UUID;
 
@@ -10,8 +13,17 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class KafkaMessageRequestResultDto {
+  @NotNull(message = "Параметр requestResultId должен быть заполнен!")
   private UUID requestResultId;
+
+  @NotNull(message = "Параметр userId должен быть заполнен!")
   private UUID userId;
+
+  @NotEmpty(message = "Параметр url должен быть заполнен!")
+  @CheckSqlInjection
   private String url;
+
+  @NotEmpty(message = "Параметр domain должен быть заполнен!")
+  @CheckSqlInjection
   private String domain;
 }
